e4054c7eeabd6b57811b9c2f2eab33049e0f15ed
Patrick <ppevans11@gmail.com>
2019-02-12 18:54:27 +1300

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -37,42 +37,8 @@ class Palette {
 
 // Application entry point
 void main() => runApp(
-  //GravityDigApp()
   GravityDigGame().widget
 );
-
-// GravityDigApp stateless widget class
-class GravityDigApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gravity Dig Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.green,
-      ),
-      home: HomePage(),
-    );
-  }
-} // END of GravityDigApp
-
-
-// HomePage stateless widget class
-class HomePage extends StatelessWidget {
-  // METHODS
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Gravity Dig Home Page'),
-        ],
-      ),
-    );
-  }
-} // END of HomePage
 
 
 // SimpleComponent class
@@ -98,8 +64,6 @@ class SimpleComponent extends PositionComponent {
 class GravityDigGame extends BaseGame {
   // CONSTRUCTOR
   GravityDigGame() {
-    // for testing
-    print("Hello Gravity Dig");
     // enter fullscreen mode
     Flame.util.fullScreen();
     // start the game
@@ -110,6 +74,7 @@ class GravityDigGame extends BaseGame {
     add(SimpleComponent());
   }
 } // END of GravityDigGame
+
 
 // EOF
 
