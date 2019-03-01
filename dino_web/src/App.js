862e943bed3b991a1f2a4c3e2609ada80f6b0824
Patrick <ppevans11@gmail.com>
2019-03-01 20:29:27 +1300

    added p5 sketch

--- dino_web/src/App.js
+++ dino_web/src/App.js
@@ -5,16 +5,27 @@
 
 // IMPORTS
 import React, { Component } from 'react';
+// p5 react wrapper
+import P5Wrapper from 'react-p5-wrapper';
+// p5 sketch
+import sketch from './sketch'
+// styles
 import './App.css';
 
 // Main app component class
 class App extends Component {
+
+  rotation = 24;
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>header.</p>
+          <h1>Dino Dash</h1>
         </header>
+        <div>
+          <P5Wrapper sketch={sketch} rotation={this.rotation} />
+        </div>
       </div>
     );
   }
