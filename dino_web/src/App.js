756236d4eba8d5152067eb3f4de2838208dd4119
Patrick <ppevans11@gmail.com>
2019-03-01 21:11:39 +1300

    updates to sketch

--- dino_web/src/App.js
+++ dino_web/src/App.js
@@ -9,7 +9,6 @@ import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 // p5 sketches
 import sketch from './sketches/sketch'
-import sketch1 from './sketches/sketch1'
 // styles
 import './App.css';
 
@@ -20,17 +19,12 @@ class App extends Component {
 		super(props);
 		this.state = {
 			rotation: 150,
-			stateSketch: sketch,
 		};
   }
   
 	rotationChange(e){
 		this.setState({rotation:e.target.value});
   }
-  
-	pressEvent(){
-		this.state.stateSketch === sketch ? this.setState({stateSketch:sketch1}) : this.setState({stateSketch:sketch});
-	}
 
   render() {
     return (
@@ -39,44 +33,17 @@ class App extends Component {
           <h1>Dino Dash</h1>
         </header>
         <div>  
-          <P5Wrapper sketch={this.state.stateSketch} rotation={this.state.rotation}/>
-          <input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
-          <button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
+          <P5Wrapper sketch={sketch} rotation={this.state.rotation}/>        
+  				<input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
         </div>
       </div>
     );
   }
+
 }
 
 export default App;
 
-/*
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			rotation: 150,
-			stateSketch: sketch,
-		};
-	}
-
-	rotationChange(e){
-		this.setState({rotation:e.target.value});
-	}
-
-	pressEvent(){
-		this.state.stateSketch === sketch ? this.setState({stateSketch:sketch2}) : this.setState({stateSketch:sketch});
-	}
-
-	render () {
-		return (
-			<div>
-				<P5Wrapper sketch={this.state.stateSketch} rotation={this.state.rotation}/>
-				<input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
-				<button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
-			</div>
-		);
-	}
-
-*/
+// EOF
 
