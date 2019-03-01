e8f3abc1371b8a5797ddbc644fec5412b69b6689
Patrick <ppevans11@gmail.com>
2019-03-01 21:00:06 +1300

    updates to p5 sketches

--- dino_web/src/App.js
+++ dino_web/src/App.js
@@ -7,15 +7,30 @@
 import React, { Component } from 'react';
 // p5 react wrapper
 import P5Wrapper from 'react-p5-wrapper';
-// p5 sketch
-import sketch from './sketch'
+// p5 sketches
+import sketch from './sketches/sketch'
+import sketch1 from './sketches/sketch1'
 // styles
 import './App.css';
 
 // Main app component class
 class App extends Component {
 
-  rotation = 24;
+  constructor(props) {
+		super(props);
+		this.state = {
+			rotation: 150,
+			stateSketch: sketch,
+		};
+  }
+  
+	rotationChange(e){
+		this.setState({rotation:e.target.value});
+  }
+  
+	pressEvent(){
+		this.state.stateSketch === sketch ? this.setState({stateSketch:sketch1}) : this.setState({stateSketch:sketch});
+	}
 
   render() {
     return (
@@ -23,8 +38,10 @@ class App extends Component {
         <header className="App-header">
           <h1>Dino Dash</h1>
         </header>
-        <div>
-          <P5Wrapper sketch={sketch} rotation={this.rotation} />
+        <div>  
+          <P5Wrapper sketch={this.state.stateSketch} rotation={this.state.rotation}/>
+          <input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
+          <button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
         </div>
       </div>
     );
@@ -32,3 +49,34 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			rotation: 150,
+			stateSketch: sketch,
+		};
+	}
+
+	rotationChange(e){
+		this.setState({rotation:e.target.value});
+	}
+
+	pressEvent(){
+		this.state.stateSketch === sketch ? this.setState({stateSketch:sketch2}) : this.setState({stateSketch:sketch});
+	}
+
+	render () {
+		return (
+			<div>
+				<P5Wrapper sketch={this.state.stateSketch} rotation={this.state.rotation}/>
+				<input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
+				<button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
+			</div>
+		);
+	}
+
+*/
+
--- dino_web/src/sketches/sketch.js
+++ dino_web/src/sketches/sketch.js
@@ -22,9 +22,19 @@ export default function sketch (p) {
     p.draw = function () {
         p.background(100);
         p.noStroke();
+    
         p.push();
+        p.translate(-150, 100);
         p.rotateY(rotation);
+        p.rotateX(-0.9);
         p.box(100);
+        p.pop();
+    
+        p.noFill();
+        p.stroke(255);
+        p.push();
+        p.translate(500, p.height*0.35, -200);
+        p.sphere(300);
         p.pop();
     };
 };
